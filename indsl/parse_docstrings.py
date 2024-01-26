6d82433903b279fc19d6a1b2df1f452ebe903347
Vebjørn Gilberg <vebjorngilberg@gmail.com>
2024-01-26 12:04:37 +0100

    refactoring

--- indsl/parse_docstrings.py
+++ indsl/parse_docstrings.py
@@ -34,8 +34,8 @@ def _create_key(
     function_name: Optional[str] = None,
     description: Optional[bool] = False,
     parameter: Optional[str] = None,
-    return_name: Optional[str] = None,
-    version: Optional[str] = "",
+    output: Optional[bool] = False,
+    version: Optional[str] = None,
 ) -> Optional[str]:
     elements = [PREFIX]
 
@@ -45,7 +45,7 @@ def _create_key(
     else:
         if function_name:
             elements.append(_format_str(function_name))
-            if return_name:
+            if output:
                 elements.append(RETURN)
             elif parameter:
                 elements.append(_format_str(parameter))
@@ -78,15 +78,17 @@ def _convert_to_rendering_format(docstring: str) -> str:
 
 
 # Generate parameters
-def _generate_parameters(name: str, output_dict: dict, parameters, version: Optional[str] = None):
+def _generate_key_for_parameters(name: str, output_dict: dict, parameters, version: Optional[str] = None):
     for parameter in parameters:
         param_name, description = _parse_docstring_element_text(parameter.description)
         if param_name:
             key = _create_key(function_name=name, parameter=parameter.arg_name, version=version)
             output_dict[key] = param_name
         if description:
-            desc_key = _create_key(function_name=name, parameter=parameter.arg_name, description=True, version=version)
-            output_dict[desc_key] = _convert_to_rendering_format(description)
+            description_key = _create_key(
+                function_name=name, parameter=parameter.arg_name, description=True, version=version
+            )
+            output_dict[description_key] = _convert_to_rendering_format(description)
 
 
 # TODO: REFACTOR AND FIGURE OUT WHY TOOLBOX_NAME IS INCLUDED IN THE JSON FILE. FIGURE OUT WHY VERSIONING FUNCTION APPEARS IN THE JSON.
@@ -99,7 +101,7 @@ def _generate_key_for_function(function: typing.Callable, name: str, output_dict
 
     # Function name
     short_description = parsed_docstring.short_description or ""
-    output_dict[_create_key(function_name=name, version=version)] = short_description
+    output_dict[_create_key(function_name=name, version=version)] = _parse_docstring_element_text(short_description)[0]
 
     # Function description
     if parsed_docstring.long_description:
@@ -108,55 +110,53 @@ def _generate_key_for_function(function: typing.Callable, name: str, output_dict
 
     # Function parameters
     if parsed_docstring.params:
-        _generate_parameters(name, output_dict, parsed_docstring.params, version)
+        _generate_key_for_parameters(name, output_dict, parsed_docstring.params, version)
 
     # Function return
     if parsed_docstring.returns:
         return_name = _parse_docstring_element_text(parsed_docstring.returns.description)[0]
         if return_name:
-            return_key = _create_key(function_name=name, return_name=return_name, version=version)
+            return_key = _create_key(function_name=name, output=True, version=version)
             output_dict[return_key] = return_name
 
 
 # Add versioning if the function is versioned
-def _generate_function_name_and_versioning(output_dict: dict, module):
+def _generate_translation_mapping_for_functions(output_dict: dict, module):
     functions_to_export = getattr(module, COGNITE, [])
     functions_map = inspect.getmembers(module, inspect.isfunction)
 
     for name, function in filter(lambda f: f[0] in functions_to_export, functions_map):
         if versioning.is_versioned(function):
-            _process_versioned_function(function, name, output_dict)
+            _generate_key_for_versioned_function(function, name, output_dict)
         else:
             _generate_key_for_function(function, name, output_dict=output_dict)
 
 
 # Process versioned function by adding only the deprecated version to the keys.
 # The latest version does not have a version number in the key.
-def _process_versioned_function(function, name, output_dict):
+def _generate_key_for_versioned_function(function, name, output_dict):
     function_name = versioning.get_name(function)
     versions = versioning.get_versions(function_name)
 
     for version in versions:
         func = versioning.get(function_name, version)
-        current_function = func if version != versions[-1] else function
         _generate_key_for_function(
-            current_function,
-            function_name if version != versions[-1] else name,
+            func,
+            function_name,
             output_dict=output_dict,
             version=version if version != versions[-1] else None,
         )
 
 
 # Write the keys and values to the JSON file
-def _docstring_to_json(module):
+def create_mapping_for_translations(module):
     output_dict = {}
     for _, module in inspect.getmembers(indsl, inspect.ismodule):
         toolbox_name = getattr(module, TOOLBOX_NAME, None)
-        if toolbox_name is not None:
+        if toolbox_name is not None and toolbox_name != "Not listed operations":
             output_dict[_create_key(toolbox=toolbox_name)] = toolbox_name
-
-        # extract doctring from each function
-        _generate_function_name_and_versioning(output_dict, module)
+            # extract doctring from each function
+            _generate_translation_mapping_for_functions(output_dict, module)
 
     with open("toolboxes.json", "w") as f:
         json.dump(output_dict, f, indent=4)
