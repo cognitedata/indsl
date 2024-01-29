7cc5f8deba422ea390df89fac23967161d8b6c8c
Vebjørn Gilberg <vebjorngilberg@gmail.com>
2024-01-29 09:58:37 +0100

    reorganize files

--- tests/test_parse_docstrings.py
+++ tests/test_parse_docstrings.py
@@ -1,18 +1,31 @@
 import os
 
-import indsl.parse_docstrings as parse_docstrings
+import translations.create_translation_json as create_translation
 
 
 def test_json_file_creation():
-    # Define a test function with a docstring
-    def test_function():
-        """This is a test function."""
-        pass
+    # Define a test function with a docstring with parameters, return value and description
+    def test_function(test_parameter: str, test_return: str = "test_return") -> str:
+        """Test function.
+
+        This is a long description of the test function. It can contain multiple lines. It can also contain
+        links (https://www.cognite.com).
+
+        Args:
+            test_parameter: Test parameter.
+                Test parameter description.
+            test_return: Test return.
+                Test return description.
+
+        Returns:
+            Test return.
+        """
+        return test_return
 
     # Create a test module that contains the test function
-    test_module = type("test_module", (object,), {"test_function": test_function})
+    test_module = type("test_module", (object,), {"test_function": test_function("test_parameter")})
 
-    parse_docstrings.create_mapping_for_translations(test_module)
+    create_translation.create_mapping_for_translations(test_module)
 
     # Check whether the expected JSON file has been created
-    assert os.path.isfile("toolboxes.json")
+    assert os.path.isfile("translated_toolboxes.json")
--- translations/create_translation_json.py
+++ translations/create_translation_json.py
@@ -12,50 +12,16 @@ from docstring_to_markdown.rst import rst_to_markdown
 import indsl
 
 from indsl import versioning
+from indsl.create_translation_key import create_key
 
 
-PREFIX = "INDSL"
-PARAMETER = "PARAMETER"
-DESCRIPTION = "DESCRIPTION"
-RETURN = "RETURN"
 TOOLBOX_NAME = "TOOLBOX_NAME"
-TOOLBOX = "TOOLBOX"
 COGNITE = "__cognite__"
 
 
 # Format the string to uppercase and replace spaces with underscores
 def _format_str(s: Optional[str]) -> str:
     return s.upper().replace(" ", "_") if s else ""
-
-
-# Create keys for the JSON file
-def _create_key(
-    toolbox: Optional[str] = None,
-    function_name: Optional[str] = None,
-    description: Optional[bool] = False,
-    parameter: Optional[str] = None,
-    output: Optional[bool] = False,
-    version: Optional[str] = None,
-) -> Optional[str]:
-    elements = [PREFIX]
-
-    if toolbox:
-        elements.append(TOOLBOX)
-        elements.append(_format_str(toolbox))
-    else:
-        if function_name:
-            elements.append(_format_str(function_name))
-            if output:
-                elements.append(RETURN)
-            elif parameter:
-                elements.append(_format_str(parameter))
-                if description:
-                    elements.append(DESCRIPTION)
-            elif description:
-                elements.append(DESCRIPTION)
-
-    version_str = f"_{_format_str(version)}" if version else ""
-    return "_".join(elements) + version_str if elements else None
 
 
 # Parse the docstring element text
@@ -82,10 +48,10 @@ def _generate_key_for_parameters(name: str, output_dict: dict, parameters, versi
     for parameter in parameters:
         param_name, description = _parse_docstring_element_text(parameter.description)
         if param_name:
-            key = _create_key(function_name=name, parameter=parameter.arg_name, version=version)
+            key = create_key(function_name=name, parameter=parameter.arg_name, version=version)
             output_dict[key] = param_name
         if description:
-            description_key = _create_key(
+            description_key = create_key(
                 function_name=name, parameter=parameter.arg_name, description=True, version=version
             )
             output_dict[description_key] = _convert_to_rendering_format(description)
@@ -101,11 +67,11 @@ def _generate_key_for_function(function: typing.Callable, name: str, output_dict
 
     # Function name
     short_description = parsed_docstring.short_description or ""
-    output_dict[_create_key(function_name=name, version=version)] = _parse_docstring_element_text(short_description)[0]
+    output_dict[create_key(function_name=name, version=version)] = _parse_docstring_element_text(short_description)[0]
 
     # Function description
     if parsed_docstring.long_description:
-        long_desc_key = _create_key(function_name=name, description=True, version=version)
+        long_desc_key = create_key(function_name=name, description=True, version=version)
         output_dict[long_desc_key] = _convert_to_rendering_format(parsed_docstring.long_description)
 
     # Function parameters
@@ -116,7 +82,7 @@ def _generate_key_for_function(function: typing.Callable, name: str, output_dict
     if parsed_docstring.returns:
         return_name = _parse_docstring_element_text(parsed_docstring.returns.description)[0]
         if return_name:
-            return_key = _create_key(function_name=name, output=True, version=version)
+            return_key = create_key(function_name=name, output=True, version=version)
             output_dict[return_key] = return_name
 
 
@@ -127,14 +93,14 @@ def _generate_translation_mapping_for_functions(output_dict: dict, module):
 
     for name, function in filter(lambda f: f[0] in functions_to_export, functions_map):
         if versioning.is_versioned(function):
-            _generate_key_for_versioned_function(function, name, output_dict)
+            _generate_key_for_versioned_function(function, output_dict)
         else:
             _generate_key_for_function(function, name, output_dict=output_dict)
 
 
 # Process versioned function by adding only the deprecated version to the keys.
 # The latest version does not have a version number in the key.
-def _generate_key_for_versioned_function(function, name, output_dict):
+def _generate_key_for_versioned_function(function, output_dict):
     function_name = versioning.get_name(function)
     versions = versioning.get_versions(function_name)
 
@@ -155,9 +121,9 @@ def create_mapping_for_translations(module):
     for _, module in inspect.getmembers(indsl, inspect.ismodule):
         toolbox_name = getattr(module, TOOLBOX_NAME, None)
         if toolbox_name is not None and toolbox_name != "Not listed operations":
-            output_dict[_create_key(toolbox=toolbox_name)] = toolbox_name
+            output_dict[create_key(toolbox=toolbox_name)] = toolbox_name
             # extract doctring from each function
             _generate_translation_mapping_for_functions(output_dict, module)
 
-    with open("toolboxes.json", "w") as f:
+    with open("translated_toolboxes.json", "w") as f:
         json.dump(output_dict, f, indent=4)
