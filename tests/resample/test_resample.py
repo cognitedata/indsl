5e6c45ccfb68699aa234f11627cbaf95a0919d85
janne123456789 <janne.nesse@cognite.com>
2024-02-08 11:39:42 +0100

    update test and fix linting

--- tests/resample/test_resample.py
+++ tests/resample/test_resample.py
@@ -48,10 +48,12 @@ def test_all_nan_data():
 @pytest.mark.core
 def test_positive_granularity():
     with pytest.raises(UserValueError) as e:
-        test_data = pd.Series([random.random() * i for i in range(24)], index=pd.date_range("2020-02-03", periods=24, freq="1H"))
+        test_data = pd.Series(
+            [random.random() * i for i in range(24)], index=pd.date_range("2020-02-03", periods=24, freq="1H")
+        )
         granularity = pd.Timedelta(0, "m")
         resample_to_granularity(test_data, granularity=granularity)
-        assert "Timedelta must be strictly positive. The smallest possible value is '1s'" in str(e.value)
+        assert "Timedelta must be strictly positive. The smallest possible value is '1ns'" in str(e.value)
 
 # test correct output size
 @pytest.mark.parametrize("test_length", [(200), (10)])
