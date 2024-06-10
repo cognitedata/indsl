22324e0bfc7b25383ecfeb3d1bb7a6e99383adc6
Vebjørn Gilberg <vebjorngilberg@gmail.com>
2024-06-10 21:06:29 +0200

    add example

---
+++ examples/equipment/plot_mean_time_between_failures.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from indsl.equipment.mean_time_between_failures_ import mean_time_between_failures
+
+mean_time_to_failure = pd.Series([1] * 365, index=pd.date_range(start="2023-01-01", periods=365, freq="D"))
+mean_time_to_resolution = pd.Series([1] * 365, index=pd.date_range(start="2023-01-01", periods=365, freq="D"))
+
+
+mtbf = mean_time_between_failures(
+    mean_time_to_failure=mean_time_to_failure, mean_time_to_resolution=mean_time_to_resolution
+)
+
+plt.plot(mtbf)
+plt.xlabel("Date")
+plt.ylabel("Mean Time Between Failures [Time]")
+plt.title("Mean Time Between Failures")
+plt.show()
