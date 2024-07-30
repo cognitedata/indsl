7ea8753e524c5f3da32a504481be523ed2a73d8e
Gunnar Staff <gunnar.staff@cognite.com>
2024-07-30 16:40:04 +0800

    Functions operating on all elements in the time series
    Added statistical calculations like time weighted mean, time weighted standard deviation and min/max operators that work over all the values in the time series and not element wise

---
+++ indsl/ts_utils/array_operations.py
@@ -0,0 +1,127 @@
+import numpy as np
+
+# Simple operations
+import pandas as pd
+
+from indsl.exceptions import UserValueError
+from indsl.type_check import check_types
+
+
+@check_types
+def time_weighted_mean(data: pd.Series) -> pd.Series:
+    """Time weighted mean.
+
+    The time weighted mean for a time series. The returned timeseries has a constant value
+
+    Args:
+        data: Time series.
+
+    Returns:
+        pandas.Series: Time weighted mean.
+    """
+    from scipy.integrate import trapezoid
+
+    n = len(data)
+    if n == 0:
+        raise UserValueError("Expected at least one item in data parameter, got zero instead.")
+    # We need to convert the datetime to timestamp
+    timestamps = np.array([val.timestamp() for val in data.index])
+
+    # integrate over the time series to get the sum
+    timeseries_sum = trapezoid(data.values, x=timestamps)
+    # scale by the time range for the integration, giving the time weighted average
+    timeseries_average = timeseries_sum / (timestamps[-1] - timestamps[0])
+    # We need to convert it into a pandas Series
+    time_weighted_mean = pd.Series(timeseries_average * np.ones(data.shape), index=data.index)
+
+    return time_weighted_mean
+
+
+@check_types
+def time_weighted_std(data: pd.Series) -> pd.Series:
+    """Time weighted std.
+
+    The time weighted standard deviation for a time series. The returned timeseries has a constant value
+
+
+    Args:
+        data: Time series.
+
+    Returns:
+        pandas.Series: Time weighted std.
+    """
+    from scipy.integrate import trapezoid
+
+    n = len(data)
+    if n == 0:
+        raise UserValueError("Expected at least one item in data parameter, got zero instead.")
+
+    # We need to convert the datetime to timestamp
+    timestamps = np.array([val.timestamp() for val in data.index])
+
+    # first get the mean value
+    # integrate over the time series to get the sum
+    timeseries_sum = trapezoid(data.values, x=timestamps)
+    # scale by the time range for the integration, giving the time weighted average
+    timeseries_average = timeseries_sum / (timestamps[-1] - timestamps[0])
+
+    # Then calculate the stadard deviation
+    timeseries_sum = trapezoid((data.values - timeseries_average), x=timestamps)
+
+    # Then calculate the stadard deviation
+    timeseries_std = np.sqrt(trapezoid((data.values - timeseries_average), x=timestamps))
+
+    # We need to convert it into a pandas Series
+    time_weighted_std = pd.Series(timeseries_std * np.ones(data.shape), index=data.index)
+
+    return time_weighted_std
+
+
+@check_types
+def timeseries_min(data: pd.Series) -> pd.Series:
+    """Minimum value for the time series.
+
+    The returned timeseries has a constant value
+
+    Args:
+        data: Time series.
+
+    Returns:
+        pandas.Series: Minimum value timeseries.
+    """
+    n = len(data)
+    if n == 0:
+        raise UserValueError("Expected at least one item in data parameter, got zero instead.")
+
+    # Extract the smallest value
+    timeseries_min = data.min()
+
+    # We need to convert it into a pandas Series
+    timeseries_min_series = pd.Series(timeseries_min * np.ones(data.shape), index=data.index)
+
+    return timeseries_min_series
+
+
+@check_types
+def timeseries_max(data: pd.Series) -> pd.Series:
+    """Maximum value for the time series.
+
+    The returned timeseries has a constant value
+
+    Args:
+        data: Time series.
+
+    Returns:
+        pandas.Series: Maximum value timeseries.
+    """
+    n = len(data)
+    if n == 0:
+        raise UserValueError("Expected at least one item in data parameter, got zero instead.")
+
+    # Extract the smallest value
+    timeseries_max = data.max()
+
+    # We need to convert it into a pandas Series
+    timeseries_max_series = pd.Series(timeseries_max * np.ones(data.shape), index=data.index)
+
+    return timeseries_max_series
---
+++ tests/ts_utils/test_array_operations.py
@@ -0,0 +1,92 @@
+# Copyright 2021 Cognite AS
+import numpy as np
+import pandas as pd
+import math
+import pytest
+
+from indsl.ts_utils import time_weighted_mean, time_weighted_std, timeseries_min, timeseries_max
+
+# from indsl.ts_utils.operators import arithmetic_mean_many
+
+
+@pytest.mark.core
+def test_time_weighted_mean():
+    WHP_series = pd.read_pickle("./tests/ts_utils/pd_series_WHP.pkl")
+
+    from scipy.integrate import trapezoid
+
+    # We need to convert the datetime to timestamp
+    timestamps = np.array([val.timestamp() for val in WHP_series.index])
+    # integrate over the time series to get the sum
+    timeseries_sum = trapezoid(WHP_series.values, x=timestamps)
+    # scale by the time range for the integration, giving the time weighted average
+    timeseries_average = timeseries_sum / (timestamps[-1] - timestamps[0])
+
+    time_weighted_mean_indsl = time_weighted_mean(WHP_series)
+    assert math.isclose(time_weighted_mean_indsl.values[0] - timeseries_average, 0, abs_tol=1e-8)
+
+
+@pytest.mark.core
+def test_time_weighted_std():
+    WHP_series = pd.read_pickle("./tests/ts_utils/pd_series_WHP.pkl")
+
+    from scipy.integrate import trapezoid
+
+    # We need to convert the datetime to timestamp
+    timestamps = np.array([val.timestamp() for val in WHP_series.index])
+
+    # first get the mean value
+    # integrate over the time series to get the sum
+    timeseries_sum = trapezoid(WHP_series.values, x=timestamps)
+    # scale by the time range for the integration, giving the time weighted average
+    timeseries_average = timeseries_sum / (timestamps[-1] - timestamps[0])
+
+    # Then calculate the stadard deviation
+    timeseries_sum = trapezoid((WHP_series.values - timeseries_average), x=timestamps)
+
+    # Then calculate the stadard deviation
+    timeseries_std = np.sqrt(trapezoid((WHP_series.values - timeseries_average), x=timestamps))
+
+    time_weighted_std_indsl = time_weighted_std(WHP_series)
+    assert math.isclose(time_weighted_std_indsl.values[0] - timeseries_std, 0, abs_tol=1e-8)
+
+
+@pytest.mark.core
+def test_timeseries_min():
+    WHP_series = pd.read_pickle("./tests/ts_utils/pd_series_WHP.pkl")
+
+    timeseries_min_test = WHP_series.min()
+
+    timeseries_min_indsl = timeseries_min(WHP_series)
+    assert math.isclose(timeseries_min_indsl.values[0] - timeseries_min_test, 0, abs_tol=1e-8)
+
+
+@pytest.mark.core
+def test_timeseries_max():
+    WHP_series = pd.read_pickle("./tests/ts_utils/pd_series_WHP.pkl")
+
+    timeseries_max_test = WHP_series.max()
+
+    timeseries_max_indsl = timeseries_max(WHP_series)
+    assert math.isclose(timeseries_max_indsl.values[0] - timeseries_max_test, 0, abs_tol=1e-8)
+
+
+# test_time_weighted_mean()
+
+# assert add(5, 70) == 75
+
+# # Add arrays
+# num1 = np.array([5, 70])
+# num2 = np.array([70, 5])
+# assert add(num1, num2).mean() == 75
+
+# # Add pandas series
+# arr1 = pd.Series(data=np.array([5, 6, 7]))
+# arr2 = pd.Series(data=np.array([70, 69, 68]))
+# assert add(arr1, arr2).mean() == 75
+
+# # Add pandas data frame
+# df1 = pd.DataFrame(data={"a": np.array([5, 6, 7]), "b": np.array([70, 69, 68])})
+# df2 = pd.DataFrame(data={"a": np.array([70, 69, 68]), "b": np.array([5, 6, 7])})
+# assert add(df1.a, df1.b).mean() == 75
+# assert add(df1, df2).mean().mean() == 75
