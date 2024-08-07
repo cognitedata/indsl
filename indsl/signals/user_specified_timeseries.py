811154c6bd1b1978ed593844f00a5facba1f3ce5
Gunnar Staff <gunnar.staff@cognite.com>
2024-08-07 15:15:52 +0800

    Added the posibility to accept the input as a list of strings in a datetime format

--- indsl/signals/user_specified_timeseries.py
+++ indsl/signals/user_specified_timeseries.py
@@ -4,13 +4,12 @@ from typing import List
 import pandas as pd
 
 from indsl.exceptions import UserValueError
-from indsl.type_check import check_types
 
 
 # from indsl.validations import validate_series_is_not_empty
 
 
-@check_types
+# @check_types
 def user_specified_timeseries(
     input_time: List[float] = [0.0, 1.0], input_values: List[float] = [0.0, 1.0]
 ) -> pd.Series:
@@ -31,9 +30,15 @@ def user_specified_timeseries(
         raise UserValueError("There is a different number of timestamps and values. len(x)=%d,len(x)=%d" % (n_t, n_y))
     if n_t < 1:
         raise UserValueError("Empty input. We need at least one point to show")
-    # Convert input timestamps to datetime
+    # time will be spesified as a pd.Timestamp
+    # First check if it is a string or a flot.
     timestamps = []
-    for time_ in input_time:
-        timestamps.append(pd.Timestamp(time_, unit="s"))
+    if input_time[0] is str:
+        for time_ in input_time:
+            timestamps.append(pd.Timestamp(time_))
+    else:
+        # Convert input timestamps to datetime
+        for time_ in input_time:
+            timestamps.append(pd.Timestamp(time_, unit="s"))
     output_series = pd.Series(input_values, index=timestamps)
     return output_series
--- tests/signals/test_user_specified_timeseries.py
+++ tests/signals/test_user_specified_timeseries.py
@@ -8,6 +8,7 @@ from indsl.signals.user_specified_timeseries import user_specified_timeseries
 
 @pytest.mark.core
 def test_user_specified_timeseries():
+    """Timestamp given as Unix time in sec"""
     import pickle
 
     input_time = np.arange(1722470401, 1722643201, 60)  # every minute for two days
@@ -22,3 +23,72 @@ def test_user_specified_timeseries():
     ts_compare = pd.Series(input_y, index=timestamps)
 
     assert_series_equal(ts_series, ts_compare)
+
+
+@pytest.mark.core
+def test_user_specified_timeseries_2():
+    """Timestamp given as a string in datetime format"""
+    import pickle
+
+    input_time = np.arange(1722470401, 1722643201, 60)  # every minute for two days
+    input_y = np.sin((input_time - input_time[0]) / (input_time[-1] - input_time[0]) * 2 * np.pi)
+
+    input_time_str = [pd.Timestamp(time_, unit="s").strftime("%Y-%m-%d %X") for time_ in input_time]
+    # print(tmp)
+
+    ts_series = user_specified_timeseries(input_time_str, input_y.tolist())
+
+    # Manually create the series
+    # Convert input timestamps to datetime
+    timestamps = []
+    for time_ in input_time_str:
+        timestamps.append(pd.Timestamp(time_))
+    ts_compare = pd.Series(input_y, index=timestamps)
+
+    assert_series_equal(ts_series, ts_compare)
+
+
+@pytest.mark.core
+def test_user_specified_timeseries_3():
+    """Timestamp given as a string in datetime different format"""
+    import pickle
+
+    input_time_str = [
+        "8-1-24 0:00",
+        "8-1-24 0:10",
+        "8-1-24 0:20",
+        "8-1-24 0:30",
+        "8-1-24 0:40",
+        "8-1-24 0:50",
+        "8-1-24 1:00",
+        "8-1-24 1:10",
+        "8-1-24 1:20",
+        "8-1-24 1:30",
+        "8-1-24 1:40",
+        "8-1-24 1:50",
+        "8-1-24 2:00",
+        "8-1-24 2:10",
+        "8-1-24 2:20",
+        "8-1-24 2:30",
+        "8-1-24 2:40",
+        "8-1-24 2:50",
+    ]
+    input_time_pd = [pd.Timestamp(time_) for time_ in input_time_str]  #
+    # print(input_time_pd)
+    # Convert to timestamps
+    input_time = np.array([time_.timestamp() for time_ in input_time_pd])
+    input_y = np.sin((input_time - input_time[0]) / (input_time[-1] - input_time[0]) * 2 * np.pi)
+
+    ts_series = user_specified_timeseries(input_time_str, input_y.tolist())
+
+    # Manually create the series
+    # Convert input timestamps to datetime
+    timestamps = []
+    for time_ in input_time_str:
+        timestamps.append(pd.Timestamp(time_))
+    ts_compare = pd.Series(input_y, index=input_time_pd)
+
+    assert_series_equal(ts_series, ts_compare)
+
+
+# test_user_specified_timeseries_3()
