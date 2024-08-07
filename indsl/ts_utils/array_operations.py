Fixed a bug in the std calculation. Sanitized the test data

indsl/ts_utils/array_operations.py
@@ -26,6 +26,9 @@ def time_weighted_mean(data: pd.Series) -> pd.Series:
 
     # We need to convert the datetime to timestamp
     timestamps = np.array([val.timestamp() for val in data.index])
+    # We normalise as well to reduce rounding errors, since the timestamp is usually a very large number
+    timestamps = timestamps - timestamps[0]
+    timestamps = timestamps / timestamps[-1]
 
     # integrate over the time series to get the sum
     timeseries_sum = trapezoid(data.values, x=timestamps)
@@ -55,20 +58,22 @@ def time_weighted_std(data: pd.Series) -> pd.Series:
     # Check if the time series is empty
     validate_series_is_not_empty(data)
 
-    # We need to convert the datetime to timestamp
+    # We need to convert the datetime to timestamp.
     timestamps = np.array([val.timestamp() for val in data.index])
+    # We normalise as well to reduce rounding errors, since the timestamp is usually a very large number
+    timestamps = timestamps - timestamps[0]
+    timestamps = timestamps / timestamps[-1]
 
     # first get the mean value
-    # integrate over the time series to get the sum
+    # integrate over the time series
     timeseries_sum = trapezoid(data.values, x=timestamps)
     # scale by the time range for the integration, giving the time weighted average
     timeseries_average = timeseries_sum / (timestamps[-1] - timestamps[0])
 
-    # Then calculate the stadard deviation
-    timeseries_sum = trapezoid((data.values - timeseries_average), x=timestamps)
-
-    # Then calculate the stadard deviation
-    timeseries_std = np.sqrt(trapezoid((data.values - timeseries_average), x=timestamps))
+    # Then calculate the time-weighted standard deviation
+    timeseries_std = np.sqrt(
+        trapezoid((data.values - timeseries_average) ** 2, x=timestamps) / (timestamps[-1] - timestamps[0])
+    )
 
     # We need to convert it into a pandas Series
     time_weighted_std = pd.Series(timeseries_std * np.ones(data.shape), index=data.index)

tests/ts_utils/test_array_operations.py
@@ -40,10 +40,9 @@ def test_time_weighted_std():
     timeseries_average = timeseries_sum / (timestamps[-1] - timestamps[0])
 
     # Then calculate the stadard deviation
-    timeseries_sum = trapezoid((WHP_series.values - timeseries_average), x=timestamps)
-
-    # Then calculate the stadard deviation
-    timeseries_std = np.sqrt(trapezoid((WHP_series.values - timeseries_average), x=timestamps))
+    timeseries_std = np.sqrt(
+        trapezoid((WHP_series.values - timeseries_average) ** 2, x=timestamps) / (timestamps[-1] - timestamps[0])
+    )
 
     time_weighted_std_indsl = time_weighted_std(WHP_series)
     assert math.isclose(time_weighted_std_indsl.values[0] - timeseries_std, 0, abs_tol=1e-8)