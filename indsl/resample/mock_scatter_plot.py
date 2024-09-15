e668777b52ef27b1affc9cc2c9b45a07c9cf41f6
Gunnar Staff <gunnar.a.staff@gmail.com>
2024-09-15 14:33:41 +0200

    Bugfix for the mock scatter plot
    Wrong scaling of the x-values

--- indsl/resample/mock_scatter_plot.py
+++ indsl/resample/mock_scatter_plot.py
@@ -10,9 +10,7 @@ from indsl.type_check import check_types
 
 
 @check_types
-def __reindex_scatter_core(
-    signal_x: pd.Series, x_min_value: float = 0, x_max_value: float = 1
-) -> Tuple[np.array, np.array]:
+def __reindex_scatter_core(signal_x: pd.Series) -> Tuple[np.array, np.array]:
     """Reindex scatterplot core.
 
     It returns a reindexed array of timestamp. The timestamps are creates such that the values from signal_x
@@ -22,10 +20,6 @@ def __reindex_scatter_core(
     Args:
         signal_x: x-value.
             The time series where the values are used as the x-value
-        x_min_value: Minimum x value.
-            Minimum x value, used to scale the x values.
-        x_max_value: Maximum x value.
-            Maximum x value, used to scale the x value.
 
     Returns:
         np.array: Scatter plot index, Scatter plot sort index
@@ -33,8 +27,11 @@ def __reindex_scatter_core(
     # convert timestamps to epoc
     epoc = np.array([val.timestamp() for val in signal_x.index])
     d_epoc = epoc[-1] - epoc[0]
-    # We will now map the values in singla_x to the epoc and then convert it back to datetime
-    sequence_epoc = signal_x.values / (x_max_value - x_min_value) * d_epoc
+    # We will now map the values in signal_x to the epoc and then convert it back to datetime
+    x_min_value = signal_x.min()
+    x_max_value = signal_x.max()
+    # translate to [0,->] range, then scale to [0,1] and later scale to
+    sequence_epoc = (signal_x.values - x_min_value) / (x_max_value - x_min_value) * d_epoc
     index_x_epoc = sequence_epoc + epoc[0]  # translate
     index_x = np.array([datetime.fromtimestamp(epoc_) for epoc_ in index_x_epoc])
     # create a sort index, such that the order is increasing
@@ -47,8 +44,6 @@ def __reindex_scatter_core(
 def reindex_scatter(
     signal_x: pd.Series,
     signal_y: pd.Series,
-    x_min_value: float = 0,
-    x_max_value: float = 1,
     align_timesteps: bool = False,
 ) -> pd.Series:
     """Reindex scatterplot.
@@ -64,10 +59,6 @@ def reindex_scatter(
             The time series where the values are used as the x-value
         signal_y: y-value.
             The time series where the values are used as the y-value
-        x_min_value: Minimum x value.
-            Minimum x value, used to scale the x values.
-        x_max_value: Maximum x value.
-            Maximum x value, used to scale the x value.
         align_timesteps (bool) : Auto-align
             Automatically align time stamp  of input time series. Default is False.
 
@@ -76,8 +67,10 @@ def reindex_scatter(
     """
     if align_timesteps:
         signal_x, signal_y = auto_align([signal_x, signal_y], align_timesteps)
+    # print("reindex",signal_x.index)
+    # print("reindex",signal_y.index)
 
-    index_x_sorted, index_sort = __reindex_scatter_core(signal_x, x_min_value=x_min_value, x_max_value=x_max_value)
+    index_x_sorted, index_sort = __reindex_scatter_core(signal_x)
 
     signal_scatter = pd.Series(signal_y.array[index_sort], index=index_x_sorted)
 
@@ -85,7 +78,7 @@ def reindex_scatter(
 
 
 @check_types
-def reindex_scatter_x(signal_x: pd.Series, x_min_value: float = 0, x_max_value: float = 1) -> pd.Series:
+def reindex_scatter_x(signal_x: pd.Series) -> pd.Series:
     """Reindex scatterplot x-values.
 
     It returns the values from signal_y with the timestamps as the values from signal_x,
@@ -96,15 +89,11 @@ def reindex_scatter_x(signal_x: pd.Series, x_min_value: float = 0, x_max_value: 
     Args:
         signal_x: x-value.
             The time series where the values are used as the x-value
-        x_min_value: Minimum x value.
-            Minimum x value, used to scale the x values.
-        x_max_value: Maximum x value.
-            Maximum x value, used to scale the x value.
 
     Returns:
         pandas.Series: Scatter plot
     """
-    index_x_sorted, index_sort = __reindex_scatter_core(signal_x, x_min_value=x_min_value, x_max_value=x_max_value)
+    index_x_sorted, index_sort = __reindex_scatter_core(signal_x)
 
     signal_scatter_x = pd.Series(signal_x.array[index_sort], index=index_x_sorted)
 
